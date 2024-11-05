0b4ce81da820ca237826e5f39408765d4f7db3bf
Denis Fadeev <denis@fadeev.org>
2024-11-05 17:20:43 +0300

    4443

--- packages/localnet/src/tonLightClient.ts
+++ packages/localnet/src/tonLightClient.ts
@@ -1,9 +1,4 @@
-import {
-  LiteClient,
-  LiteRoundRobinEngine,
-  LiteSingleEngine,
-  LiteEngine,
-} from "ton-lite-client";
+import { LiteClient, LiteSingleEngine, LiteEngine } from "ton-lite-client";
 import { Address } from "@ton/core";
 
 let server = {
@@ -14,24 +9,25 @@ let server = {
 };
 
 async function main() {
-  const engines: LiteEngine[] = [];
-  engines.push(
-    new LiteSingleEngine({
-      host: `tcp://0.0.0.0:4443`,
+  try {
+    const engine = new LiteSingleEngine({
+      host: `http://127.0.0.1:8111`, // use 127.0.0.1 instead of 0.0.0.0
       publicKey: Buffer.from(server.id.key, "base64"),
-    })
-  );
-  const engine: LiteEngine = new LiteRoundRobinEngine(engines);
-  const client = new LiteClient({ engine });
-  console.log("get master info");
-  const master = await client.getMasterchainInfo();
-  console.log("master", master);
+    });
 
-  const address = Address.parse(
-    "kQC2sf_Hy34aMM7n9f9_V-ThHDehjH71LWBETy_JrTirPIHa"
-  );
-  const accountState = await client.getAccountState(address, master.last);
-  console.log("Account state:", accountState);
+    const client = new LiteClient({ engine });
+    console.log("get master info");
+    const master = await client.getMasterchainInfo();
+    console.log("master", master);
+
+    const address = Address.parse(
+      "kQC2sf_Hy34aMM7n9f9_V-ThHDehjH71LWBETy_JrTirPIHa"
+    );
+    const accountState = await client.getAccountState(address, master.last);
+    console.log("Account state:", accountState);
+  } catch (error) {
+    console.error("Error during execution:", error);
+  }
 }
 
 main();
