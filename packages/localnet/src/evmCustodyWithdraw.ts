0a8605702fda21d1f381fb76c143a6c749ce2809
Denis Fadeev <denis@fadeev.org>
2025-02-06 09:59:06 +0300

    handle error when withdrawing/calling from erc-20 custody

--- packages/localnet/src/evmCustodyWithdraw.ts
+++ packages/localnet/src/evmCustodyWithdraw.ts
@@ -12,24 +12,28 @@ export const evmCustodyWithdraw = async ({
   foreignCoins: any[];
   tss: any;
 }) => {
-  const zrc20 = args[3];
-  const foreignAsset = foreignCoins.find(
-    (coin: any) => coin.zrc20_contract_address === zrc20
-  );
-  if (!foreignAsset) {
-    throw new Error(`Foreign coin not found for ZRC20 address: ${zrc20}`);
+  try {
+    const zrc20 = args[3];
+    const foreignAsset = foreignCoins.find(
+      (coin: any) => coin.zrc20_contract_address === zrc20
+    );
+    if (!foreignAsset) {
+      throw new Error(`Foreign coin not found for ZRC20 address: ${zrc20}`);
+    }
+    const { asset, foreign_chain_id } = foreignAsset;
+
+    const amount = args[4];
+    const receiver = args[2];
+
+    const tx = await evmContracts[foreign_chain_id].custody
+      .connect(tss)
+      .withdraw(receiver, asset, amount, deployOpts);
+    await tx.wait();
+    log(
+      foreign_chain_id,
+      `Transferred ${amount} ERC-20 tokens from Custody to ${receiver}`
+    );
+  } catch (error: any) {
+    throw new Error(`Error withdrawing from ERC-20 custody: ${error}`);
   }
-  const { asset, foreign_chain_id } = foreignAsset;
-
-  const amount = args[4];
-  const receiver = args[2];
-
-  const tx = await evmContracts[foreign_chain_id].custody
-    .connect(tss)
-    .withdraw(receiver, asset, amount, deployOpts);
-  await tx.wait();
-  log(
-    foreign_chain_id,
-    `Transferred ${amount} ERC-20 tokens from Custody to ${receiver}`
-  );
 };
--- packages/localnet/src/evmCustodyWithdrawAndCall.ts
+++ packages/localnet/src/evmCustodyWithdrawAndCall.ts
@@ -13,29 +13,35 @@ export const evmCustodyWithdrawAndCall = async ({
   foreignCoins: any[];
   tss: any;
 }) => {
-  const [sender, , receiver, zrc20, amount, , , message, callOptions] = args;
-  const isArbitraryCall = callOptions[1];
+  try {
+    const [sender, , receiver, zrc20, amount, , , message, callOptions] = args;
+    const isArbitraryCall = callOptions[1];
 
-  const messageContext = {
-    sender: isArbitraryCall ? ethers.ZeroAddress : sender,
-  };
-  const foreignAsset = foreignCoins.find(
-    (coin: any) => coin.zrc20_contract_address === zrc20
-  );
-  if (!foreignAsset) {
-    throw new Error(`Foreign coin not found for ZRC20 address: ${zrc20}`);
-  }
-  const { asset, foreign_chain_id } = foreignAsset;
-
-  const executeTx = await evmContracts[foreign_chain_id].custody
-    .connect(tss)
-    .withdrawAndCall(
-      messageContext,
-      receiver,
-      asset,
-      amount,
-      message,
-      deployOpts
+    const messageContext = {
+      sender: isArbitraryCall ? ethers.ZeroAddress : sender,
+    };
+    const foreignAsset = foreignCoins.find(
+      (coin: any) => coin.zrc20_contract_address === zrc20
     );
-  await executeTx.wait();
+    if (!foreignAsset) {
+      throw new Error(`Foreign coin not found for ZRC20 address: ${zrc20}`);
+    }
+    const { asset, foreign_chain_id } = foreignAsset;
+
+    const executeTx = await evmContracts[foreign_chain_id].custody
+      .connect(tss)
+      .withdrawAndCall(
+        messageContext,
+        receiver,
+        asset,
+        amount,
+        message,
+        deployOpts
+      );
+    await executeTx.wait();
+  } catch (error: any) {
+    throw new Error(
+      `Error withdrawing and calling from ERC-20 custody: ${error}`
+    );
+  }
 };
