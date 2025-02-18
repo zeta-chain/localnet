2feaf546b8cdf3d6c3eae1db8a554b6f12bff6c3
Denis Fadeev <denis@fadeev.org>
2025-02-18 20:46:02 +0300

    wip

--- packages/tasks/src/solanaDeposit.ts
+++ packages/tasks/src/solanaDeposit.ts
@@ -24,13 +24,30 @@ const solanaDeposit = async (args: any) => {
     provider
   );
 
-  await gatewayProgram.methods
-    .deposit(
-      new anchor.BN(ethers.parseUnits(args.amount, 9).toString()),
-      ethers.getBytes(args.receiver)
-    )
-    .accounts({})
-    .rpc();
+  const receiverBytes = ethers.getBytes(args.receiver);
+
+  if (args.mint && args.from && args.to) {
+    await gatewayProgram.methods
+      .depositSplToken(args.amount, receiverBytes)
+      .accounts({
+        // pda: pda,
+        signer: provider.wallet.publicKey,
+        from: args.from,
+        to: args.to,
+        mintAccount: args.mint,
+        tokenProgram: args.tokenProgram,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+  } else {
+    await gatewayProgram.methods
+      .deposit(
+        new anchor.BN(ethers.parseUnits(args.amount, 9).toString()),
+        receiverBytes
+      )
+      .accounts({})
+      .rpc();
+  }
 };
 
 export const solanaDepositTask = task(
@@ -40,4 +57,8 @@ export const solanaDepositTask = task(
 )
   .addParam("receiver", "Address to deposit to")
   .addParam("amount", "Amount to deposit")
-  .addOptionalParam("mnemonic", "Mnemonic for generating a keypair");
+  .addOptionalParam("mnemonic", "Mnemonic for generating a keypair")
+  .addOptionalParam("mint", "SPL token mint address")
+  .addOptionalParam("from", "SPL token account from which tokens are withdrawn")
+  .addOptionalParam("to", "SPL token account that belongs to the PDA")
+  .addOptionalParam("tokenProgram", "SPL token program");
