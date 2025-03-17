a771bfef06313447cf978dab97595b706cbd391d
Denis Fadeev <denis@fadeev.org>
2025-03-17 18:28:00 +0300

    remove help

--- packages/commands/src/index.ts
+++ packages/commands/src/index.ts
@@ -4,9 +4,9 @@ import { checkCommand } from "./check";
 import { startCommand } from "./start";
 import { stopCommand } from "./stop";
 
-export const localnetCommand = new Command("localnet").description(
-  "Local development environment"
-);
+export const localnetCommand = new Command("localnet")
+  .description("Local development environment")
+  .helpCommand(false);
 
 localnetCommand.addCommand(startCommand);
 localnetCommand.addCommand(stopCommand);
