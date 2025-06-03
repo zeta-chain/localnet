6736bda899ba6eedacda8d5bade6bca4c3181bd6
Hernan Clich <hernan.g.clich@gmail.com>
2025-06-03 15:45:07 -0300

    Added jsdoc to sleep fn

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,3 +1,8 @@
+/**
+ * Asynchronously pauses execution for the specified duration.
+ * @param ms - The number of milliseconds to sleep
+ * @returns A Promise that resolves after the specified delay
+ */
 export async function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
