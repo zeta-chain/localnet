5492f1e198828cb7dcd35d53e1702082aad7db81
Hernan Clich <hernan.g.clich@gmail.com>
2025-06-09 12:16:15 -0300

    Improved contractCall type

--- src/utils/contracts.ts
+++ src/utils/contracts.ts
@@ -2,6 +2,6 @@ import { ethers } from "ethers";
 
 // Simple typed access to contract methods
 export const contractCall = (contract: ethers.BaseContract, method: string) =>
-  (contract as unknown as Record<string, unknown>)[method] as (
+  contract[method as keyof typeof contract] as (
     ...args: unknown[]
   ) => Promise<unknown>;
