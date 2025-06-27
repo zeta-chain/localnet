ab57a2d07402bbf50cf2aa84b638b13a4443654d
Denis Fadeev <denis@fadeev.org>
2025-06-27 18:48:34 +0300

    fix: Solana `call`, import IDL from npm (#209)

--- src/chains/solana/execute.ts
+++ src/chains/solana/execute.ts
@@ -1,10 +1,6 @@
 import * as anchor from "@coral-xyz/anchor";
-import {
-  getAssociatedTokenAddress,
-  getOrCreateAssociatedTokenAccount,
-} from "@solana/spl-token";
-import { PublicKey } from "@solana/web3.js";
-import bs58 from "bs58";
+import { getAssociatedTokenAddress } from "@solana/spl-token";
+import Gateway_IDL from "@zetachain/protocol-contracts-solana/dev/idl/gateway.json";
 import { keccak256 } from "ethereumjs-util";
 import { AbiCoder, ethers } from "ethers";
 
@@ -12,7 +8,6 @@ import { NetworkID } from "../../constants";
 import { logger } from "../../logger";
 import { sleep } from "../../utils";
 import { payer, secp256k1KeyPairTSS as tssKeyPair } from "./constants";
-import Gateway_IDL from "./idl/gateway.json";
 
 export const solanaExecute = async ({
   sender,
