76c9cf1cab3a5c5234d5870ef3d95a58bfac935a
Denis Fadeev <denis@fadeev.org>
2025-09-10 18:18:48 +0300

    refactor: remove Hardhat tasks

--- src/tasks/index.ts
+++
@@ -1,9 +0,0 @@
-export { localnetCheckTask } from "./check";
-export { localnetTask } from "./localnet";
-export { solanaBalanceTask } from "./solanaBalance";
-export { solanaDepositTask } from "./solanaDeposit";
-export { solanaDepositAndCallTask } from "./solanaDepositAndCall";
-export { localnetStopTask } from "./stop";
-export { suiBalanceTask } from "./suiBalance";
-export { suiDepositTask } from "./suiDeposit";
-export { suiDepositAndCallTask } from "./suiDepositAndCall";
